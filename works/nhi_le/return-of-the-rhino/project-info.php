49e9bf4a3722dc4629eb5fdc8387672a9849d9cd
Svyatoslav-Karabeen <hello@karabeen.com>
2018-06-13 11:25:30 +0100

    add rb's files

---
+++ works/nhi_le/return-of-the-rhino/project-info.php
@@ -0,0 +1,24 @@
+<?php
+
+$firstName          = 'Nhi';
+$lastName           = 'Le';
+$sex                = 'female';
+
+$hasMate            = 'no';
+$mateName           = '';
+
+$projectName        = 'Return of the Rhino';
+$projectType        = 'hybrid';
+$projectScope       = array('game design');
+
+$hasBannerVideo     = 'no';
+$BannerVideoURL     = '';
+
+$hasFinalSlider     = 'yes';
+$hasFinalVideo      = 'yes';
+$FinalVideoURL      = 'https://drive.google.com/file/d/1BdUrXyljmq79K_U3XQSXIGODDOYvNDMk/view?usp=sharing';
+
+$hasDesDoc          = 'no';
+$DesDocLink         = '';
+
+?>
