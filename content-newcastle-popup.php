b9e1e7022e4051cfa4b1fe437a36e9de2201f304
Svyatoslav-Karabeen <hello@karabeen.com>
2018-06-13 10:50:13 +0100

    new home files

---
+++ content-newcastle-popup.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * HOME PAGE >> CONTENT >> NEWCASTLE
+ * This is the template that displays information about reveal show on home page
+ *
+ * @author Svyatoslav Polishchuk (year 2018)
+ * @version 1.1
+ **/
+
+global $pageTitle;
+
+/**==========================================================================**/
+?>
