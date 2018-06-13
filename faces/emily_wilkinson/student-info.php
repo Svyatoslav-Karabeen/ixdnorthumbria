5ae1bc5279dfcb07813d9cf7b4784b6ed546b58f
Svyatoslav-Karabeen <hello@karabeen.com>
2018-06-13 11:47:23 +0100

    add emily's files

---
+++ faces/emily_wilkinson/student-info.php
@@ -0,0 +1,26 @@
+<?php
+
+$firstName          = 'Emily';
+$lastName           = 'Wilkinson';
+$sex                = 'female';
+$title              = 'UX/UI';
+
+$bioShort           = 'Hello, I’m Emily, a driven and determined UX/UI designer from Newcastle, ready to make my mark on in the design industry. ';
+$bioFull            = 'I would consider myself a very keen and quick learner who works very well under pressure. In my past 3 years at university I have developed strong UX and UI skills working on a variety of projects across a range of medias, I’m also extremely eager to undertake challenges that constantly strengthen my skills.';
+$links = array(
+    'email'         => 'emilywilkinsondesign@gmail.com',
+    'website'       => 'http://emilywilkinsondesign.com',
+    'linkedin'      => 'https://www.linkedin.com/in/emily-wilkinson-b5b254123/',
+    'behance'       => '',
+    'dribbble'      => '',
+    'twitter'       => '',
+    'facebook'      => '',
+);
+
+$openToOffers       = 'yes';
+$openToRelocation   = 'yes';
+$currentLocation    = 'Newcastle';
+$preferableLocation = 'any';
+$preferableCompany  = 'any';
+
+?>
---
+++ works/emily_wilkinson/Enhanced-Biometric-Passports/project-info.php
@@ -0,0 +1,24 @@
+<?php
+
+$firstName          = 'Emily';
+$lastName           = 'Wilkinson';
+$sex                = 'female';
+
+$hasMate            = 'no';
+$mateName           = '';
+
+$projectName        = 'Enhanced Biometric Passports';
+$projectType        = 'Service Design';
+$projectScope       = array('user experience', 'research');
+
+$hasBannerVideo     = 'yes';
+$BannerVideoURL     = 'https://youtu.be/r0i7xa2J6PY';
+
+$hasFinalSlider     = 'yes';
+$hasFinalVideo      = 'no';
+$FinalVideoURL      = '';
+
+$hasDesDoc          = 'yes';
+$DesDocLink         = 'https://drive.google.com/file/d/1AeoIIlcJJy6GhbGnCKVXCUJlj_ULMd25/view?usp=sharing';
+
+?>
