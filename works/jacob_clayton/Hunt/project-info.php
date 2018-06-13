7ef199a118b8def0d4f7800e7e4f0680c78122e5
Svyatoslav-Karabeen <hello@karabeen.com>
2018-06-13 12:29:53 +0100

    add jacob's files

---
+++ works/jacob_clayton/Hunt/project-info.php
@@ -0,0 +1,24 @@
+<?php
+
+$firstName          = 'Jacob';
+$lastName           = 'Clayton';
+$sex                = 'male';
+
+$hasMate            = 'no';
+$mateName           = '';
+
+$projectName        = 'Hunt';
+$projectType        = 'mobile app';
+$projectScope       = array('');
+
+$hasBannerVideo     = 'no';
+$BannerVideoURL     = '';
+
+$hasFinalSlider     = 'no';
+$hasFinalVideo      = 'yes';
+$FinalVideoURL      = '';
+
+$hasDesDoc          = 'no';
+$DesDocLink         = '';
+
+?>
---
+++ works/jacob_clayton/Well-Pharmacy/project-info.php
@@ -0,0 +1,21 @@
+<?php
+
+$firstName          = 'Jacob';
+$lastName           = 'Clayton';
+$sex                = 'male';
+
+$hasMate            = 'no';
+$mateName           = '';
+
+$projectName        = 'Well Extra';
+$projectType        = 'Hybrid';
+$projectScope       = array('');
+
+$hasBannerVideo     = 'no';
+$BannerVideoURL     = '';
+
+$hasFinalSlider     = 'no';
+$hasFinalVideo      = 'yes';
+$FinalVideoURL      = '';
+
+?>
