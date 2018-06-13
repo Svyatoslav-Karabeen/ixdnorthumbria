424f4efab5d9388d980af7479d8771013f20022d
Svyatoslav-Karabeen <hello@karabeen.com>
2018-06-13 11:34:45 +0100

    add brad's files

---
+++ works/bradley_smith/LUMO/project-info.php
@@ -0,0 +1,24 @@
+<?php
+
+$firstName          = 'Bradley James';
+$lastName           = 'Smith';
+$sex                = 'male';
+
+$hasMate            = 'no';
+$mateName           = '';
+
+$projectName        = 'LUMO';
+$projectType        = 'Hybrid';
+$projectScope       = array('prototype', 'research', 'communication concept', 'user experience design' );
+
+$hasBannerVideo     = 'no';
+$BannerVideoURL     = '';
+
+$hasFinalSlider     = 'yes';
+$hasFinalVideo      = 'yes';
+$FinalVideoURL      = 'https://youtu.be/-RBTlPi-NiA';
+
+$hasDesDoc          = 'yes';
+$DesDocLink         = 'https://drive.google.com/file/d/1ABnORFE7olY_FUaKY7JYr-DPpB7pr7TM/view?usp=sharing';
+
+?>
