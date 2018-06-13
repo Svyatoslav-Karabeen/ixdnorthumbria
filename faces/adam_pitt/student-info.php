021d1cfb151bb922903f9cf5cfea39cee9acfccb
Svyatoslav-Karabeen <hello@karabeen.com>
2018-06-13 10:49:04 +0100

    add adam's files

---
+++ faces/adam_pitt/student-info.php
@@ -0,0 +1,28 @@
+<?php
+
+$firstName          = 'Adam';
+$lastName           = 'Pitt';
+$sex                = 'male';
+$title              = 'interaction';
+
+$bioShort           = 'Enthusiastic and versatile interaction designer. Take a look at my portfolio and don’t hesitate to ask any questions about me or my projects.';
+$bioFull            = 'I am a passionate and dedicated individual, with a hard-working mentality and a positive outlook in every situation. During my time at Northumbria University, I have adapted and learnt a variety of skills that I am now ready to put into practice within the work place. I will be graduating this Summer with first class honours and am currently seeking employment in London.';
+$links = array(
+    'email'         => 'a.f.pitt@hotmail.co.uk',
+    'website'       => 'https://www.adampitt.co.uk',
+    'linkedin'      => 'https://www.linkedin.com/in/adam-pitt-295781137/',
+    'behance'       => '',
+    'dribbble'      => '',
+    'twitter'       => '',
+    'facebook'      => '',
+);
+
+$openToOffers       = 'yes';
+$openToRelocation   = 'yes';
+$currentLocation    = 'Huddersfield';
+$preferableLocation = 'London';
+$preferableCompany  = 'any';
+
+
+
+?>
---
+++ works/adam_pitt/nike-designr/project-info.php
@@ -0,0 +1,24 @@
+<?php
+
+$firstName          = 'Adam';
+$lastName           = 'Pitt';
+$sex                = 'male';
+
+$hasMate            = 'no';
+$mateName           = '';
+
+$projectName        = 'Nike DESIGNR';
+$projectType        = 'VR';
+$projectScope       = array('user experience design', 'prototype', 'service design');
+
+$hasBannerVideo     = 'yes';
+$BannerVideoURL     = 'https://youtu.be/8MJBVEi0ekY';
+
+$hasFinalSlider     = 'no';
+$hasFinalVideo      = 'yes';
+$FinalVideoURL      = 'https://www.youtube.com/watch?v=SysU6ghEc3w&index=2&list=PLdX0Hrry-Aa9xYOPBAecvINi3oHX9sCct&t=1s';
+
+$hasDesDoc          = 'no';
+$DesDocLink         = '';
+
+?>
