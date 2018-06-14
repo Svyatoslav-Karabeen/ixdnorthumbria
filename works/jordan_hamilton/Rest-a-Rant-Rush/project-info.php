3d1977e7e72da9d27e235ff5eadf2e95bc1ab5c0
Svyatoslav-Karabeen <hello@karabeen.com>
2018-06-14 09:33:19 +0100

    update some project files

---
+++ works/jordan_hamilton/Rest-a-Rant-Rush/project-info.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * =============================== READ ME FIRST ===============================
+ *
+ * There are only a few things to fill, all of them are very straightforward I
+ * hope. In case of any questions just drop me a message on facebook. Thank you.
+ *
+ **/
+
+/**
+ * Just your fist, last name and your sex ('male' or 'female'), project name,
+ * project type (could be either 'hybrid', 'app', 'experiental' or 'VR')
+ * and scope of the project (add as many as you want but not too many, here is
+ * some ideas: 'logo design', 'brand identity', 'brand strategy', 'print materials',
+ * 'packaging design', 'advertising materials', 'graphic design', 'research',
+ * 'communication concept', 'mvp', 'prototype', 'user experience design').
+ **/
+
+$firstName          = 'Jordan';
+$lastName           = 'Hamilton';
+$sex                = 'Male';
+
+$projectName        = 'restARant rush';
+$projectType        = 'AR App';
+$projectScope       = array('Augmented Reality', 'Game Design', 'Logo design',);
+
+/**
+ * This is your project page settings. First, go to project page wireframes
+ * (https://invis.io/FQKKPB5VJ8N#/299713914_single_Project) to see how it will
+ * look so that you can understand what the questions are about.
+ *
+ * $hasBannerVideo  – would you like to have a video for your hero image?
+ *                    Replace the placeholder with 'yes' or 'no' only!
+ *
+ * $BannerVideoURL  – if yes, please provide a YouTube link to the video.
+ *                    Otherwise, just delete the placeholder and leave it blank.
+ *
+ *
+ * $hasFinalSlider  – would you like to have a slider for your final result
+ *                    images? Replace the placeholder with 'yes' or 'no' only!
+ *
+ * $hasBannerVideo  – would you like to have a video for your final result
+ *                    section? If there is a slider with images as well,
+ *                    the video will be shown first.
+ *
+ * $FinalVideoURL   – if yes, please provide a YouTube link to the video.
+ *                    Otherwise, just delete the placeholder and leave it blank.
+ *
+ *
+ **/
+
+$hasBannerVideo     = 'Yes';
+$BannerVideoURL     = 'assets/hero-video.mp4';
+
+$hasFinalSlider     = 'No';
+$hasFinalVideo      = 'No';
+$FinalVideoURL      = '';
+
+$hasDesDoc          = 'Yes';
+$DesDocLink         = 'assets/desdoc.pdf';
+
+?>
